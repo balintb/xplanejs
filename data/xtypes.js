3697ae781849fc9a7ce15a28ca88295dd15ec885
Balint Bartfai <balint.bartfai@me.com>
2014-11-27 13:22:41 +0000

    Adding xtypes

--- data/xtypes.js
+++ data/xtypes.js
@@ -24,6 +24,22 @@ var XTypes = {
       { name: 'side', type: 'float' }
     ]
   },
+  15: {
+    name: 'angularmoment',
+    data: [
+      { name: 'm', type: 'float' },
+      { name: 'l', type: 'float' },
+      { name: 'n', type: 'float' }
+    ]
+  },
+  16: {
+    name: 'angularvelocity',
+    data: [
+      { name: 'q', type: 'float' },
+      { name: 'p', type: 'float' },
+      { name: 'r', type: 'float' }
+    ]
+  },
   17: {
     name: 'attitude',
     data: [
@@ -33,18 +49,72 @@ var XTypes = {
       { name: 'maghdg', type: 'float' }
     ]
   },
+  18: {
+    name: 'aoa',
+    data: [
+      { name: 'alpha', type: 'float' },
+      { name: 'beta', type: 'float' },
+      { name: 'hpath', type: 'float' },
+      { name: 'vpath', type: 'float' },
+      { type: 'pad', length: 12 },
+      { name: 'slip', type: 'float' }
+    ]
+  },
+  19: {
+    name: 'compass',
+    data: [
+      { name: 'mag', type: 'float' },
+      { name: 'mavar', type: 'float' }
+    ]
+  },
+  20: {
+    name: 'globalposition',
+    data: [
+      { name: 'lat', type: 'float' },
+      { name: 'lon', type: 'float' },
+      { name: 'altmsl', type: 'float' },
+      { name: 'altagl', type: 'float' },
+      { name: 'runway', type: 'float' },
+      { name: 'altind', type: 'float' },
+      { name: 'latnorm', type: 'float' },
+      { name: 'lonnorm', type: 'float' }
+    ]
+  },
+  21: {
+    name: 'simposition',
+    data: [
+      { name: 'x', type: 'float' },
+      { name: 'y', type: 'float' },
+      { name: 'z', type: 'float' },
+      { name: 'vx', type: 'float' },
+      { name: 'vy', type: 'float' },
+      { name: 'vz', type: 'float' },
+      { name: 'distft', type: 'float' },
+      { name: 'distnm', type: 'float' }
+    ]
+  },
   37: {
     name: 'enginerpm',
     data: [
       { name: 'rpm', type: 'float' }
     ]
+  },
+  64: {
+    name: 'aeroforce',
+    data: [
+      { name: 'lift', type: 'float' },
+      { name: 'drag', type: 'float' },
+      { name: 'side', type: 'float' }
+    ]
+  },
+  65: {
+    name: 'engineforce',
+    data: [
+      { name: 'normal', type: 'float' },
+      { name: 'axial', type: 'float' },
+      { name: 'side', type: 'float' }
+    ]
   }
 };
-
-/*
-
-
-
-*/
 
 module.exports = XTypes;
